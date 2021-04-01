e60c31d09157c1cdaba47ce3bb0b386ee519c6f0
cksdnr3 <cksdnr3333@naver.com>
2021-04-02 00:03:30 +0900

    repository issue clear

--- be/demo/src/main/java/com/example/demo/uss/repository/UserRepository.java
+++ be/demo/src/main/java/com/example/demo/uss/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo.uss.repository;
 
-import java.util.Optional;
-
 import com.example.demo.uss.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,11 +8,10 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Transactional
-    @Query("select u.username, u.password from User u where u.username= :username")
-    public User login(@Param("username") String username);
+    @Query("select u from User u where username= :username")
+    User login(@Param("username") String username);
 }
